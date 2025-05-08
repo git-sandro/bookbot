910feeec82fab948c7e98702058bc5ac86f8e291
git-sandro <sandro-zimmermann@outlook.com>
2025-05-08 17:46:10 +0200

    add stats.py for functions

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+from stats import count_words
+
 def get_book_text(filepath):
     with open(filepath) as book_text:
         return book_text.read()
-
-def count_words(book_text):
-    return len(book_text.split())
 
 def main():
     words = count_words(get_book_text("books/frankenstein.txt"))
