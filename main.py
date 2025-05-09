12126a7b574c2aa6b6cfc350441c930e25faadfa
git-sandro <sandro-zimmermann@outlook.com>
2025-05-09 16:58:28 +0200

    add function count_amount_chars, which count amount of single characters in text

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from stats import count_words
+from stats import count_words, count_amount_chars
 
 def get_book_text(filepath):
     with open(filepath) as book_text:
         return book_text.read()
 
 def main():
-    words = count_words(get_book_text("books/frankenstein.txt"))
-    print(f"{words} words found in the document")
-
+    #print(f"{count_words(get_book_text('books/frankenstein.txt'))} words found in the document")
+    print(count_amount_chars(get_book_text('books/frankenstein.txt')))
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,12 @@
 def count_words(book_text):
     return len(book_text.split())
+
+def count_amount_chars(book_text):
+    chars_amount = {}
+    for i in range(len(book_text)):
+        char = book_text[i].lower()
+        if char in chars_amount:
+            chars_amount[char] += 1
+        else:
+            chars_amount[char] = 1
+    return chars_amount
